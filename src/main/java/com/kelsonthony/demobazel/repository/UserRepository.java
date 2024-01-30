e1ad9d02ec73867aadbbce600237552a0538f5ad
vagrant <vagrant@vagrant.vm>
2024-01-30 15:24:34 +0000

    update project

--- src/main/java/com/kelsonthony/demobazel/repository/UserRepository.java
+++ src/main/java/com/kelsonthony/demobazel/repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.kelsonthony.demobazel.repository;
 
 import com.kelsonthony.demobazel.domain.User;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
